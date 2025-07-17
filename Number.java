0a102e305f189c1e46c71fe1f1aacff6f068a4e2
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-17 09:10:07 -1200

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Number {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter a number : ");
+        //check user input is integer or not by scanner class method
+        if(sc.hasNextInt()){  //returns boolean value
+            int number = sc.nextInt();
+            System.out.println("You Entered an Integer");
+        }else{
+            System.out.println("Not an Integer");
+        }  
+    }
+    
+}

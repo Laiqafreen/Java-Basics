4ceb5547ebee5c433cfcd82769b217c67102bdb7
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-17 05:49:28 -1200

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum{
+    public static void main(String args[]){
+        
+        //declaration of variables with a values.
+        int a = 4;     //assigning value 4 to variable a.
+        int b = 6;    //assigning value 6 to variable b.
+        int c = 10;  //assigning value 10 to variable c.
+        int sum = a + b + c;
+        
+        //Print Sum of three numbers.
+        System.out.println("Sum of three numbers are : "+sum);
+        
+    }
+}

508326efce39a08b00f6da4c8b304ecadd4b0669
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-17 09:14:10 -1200

    Add files via upload
    Encrypts and Decrypts Messages.

---
+++ Endecrypt.java
@@ -0,0 +1,11 @@
+public class Endecrypt {
+    public static void main(String args[]){
+        int grade = 85;
+        int encryptedGrade = grade + 8 ;
+        int decryptedGrade  = encryptedGrade - 8;
+        System.out.println("Encrypted Grade = " + encryptedGrade);
+        System.out.println("Decrypted Grade = " + decryptedGrade );
+
+    }
+    
+}

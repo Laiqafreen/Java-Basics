64a84f640786ab49cc7d56dfbca92381477312f1
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-17 09:17:03 -1200

    Update km.java

--- km.java
+++ km.java
@@ -4,6 +4,7 @@ public class km {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your Kilometer unit : ");
         double kUnit = sc.nextDouble();
+        //Coversion km into miles.
         double miles = kUnit * 0.621371; 
         System.out.print("The conversion of Km to miles : " + miles);
     }    

2a2c92b0bf28d830441e51b1e60482851007c5dd
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-17 09:10:26 -1200

    Add files via upload

---
+++ Int.java
@@ -0,0 +1,9 @@
+public class Int {
+    public static void main(String args[]){
+        String s = "12345";
+        System.out.println(s);
+        //Convert string to int .
+        int n = Integer.parseInt(s); //Method of integer class.
+        System.out.println(n+1);
+    }    
+}

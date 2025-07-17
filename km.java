b272f35f073b7a4a2800e24235da24fbeb34070e
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-17 09:12:23 -1200

    Add files via upload
    Convert Kilometer into miles.

---
+++ km.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class km {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Kilometer unit : ");
+        double kUnit = sc.nextDouble();
+        double miles = kUnit * 0.621371; 
+        System.out.print("The conversion of Km to miles : " + miles);
+    }    
+}

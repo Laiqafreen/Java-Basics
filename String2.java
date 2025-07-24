877c8dcb6f1d1db64a7bfdcde0e4a9abd23e94aa
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-24 01:30:50 -1200

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,10 @@
+public class String2 {
+    public static void main(String args[]){
+
+    //return length of last word
+    String s = "Hello World!";
+    String s1 = s.substring(6,11);
+    int lastWord = s1.length();
+    System.out.println("The last word of string : "+ lastWord);
+    }
+}

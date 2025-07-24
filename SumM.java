c7507d499360679a6c0b1ed10f69aca63f24906a
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-24 01:28:54 -1200

    Add files via upload

---
+++ SumM.java
@@ -0,0 +1,13 @@
+public class SumM {
+    public static void main (String args[]){
+        int n =8;
+        int sum=0;
+        for(int i =1 ; i<=10;i++)
+        {
+            int mul=n*i;
+            sum=sum+mul;  
+        }
+        System.out.println(sum);
+    }
+    
+}

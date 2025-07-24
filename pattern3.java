331b3b3daf072f63041d585e4b0a81377b453188
Afreen Laiq <155190457+Laiqafreen@users.noreply.github.com>
2025-07-24 01:31:59 -1200

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,14 @@
+public class pattern3 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=5;j<0;j--){
+                System.out.print("*");
+            }
+             System.out.print("\n");
+            }
+        }
+        
+    }
+    
+     
+    
